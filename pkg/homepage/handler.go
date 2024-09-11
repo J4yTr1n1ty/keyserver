f40eb0b5ffddcd9286c5f71e7d48468854659fbf
Jay <jay@projectchaos.ch>
2024-09-12 00:09:38 +0200

    fix:

--- pkg/homepage/handler.go
+++ pkg/homepage/handler.go
@@ -11,6 +11,8 @@ func NewHandler() *Handler {
 }
 
 func (h *Handler) Homepage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
 	http.ServeFile(w, r, "static/index.html")
 }
